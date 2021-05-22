44a16cbe304ab789d81fd52cdfe0cba45ea7fd8e
AkashKumarGaur <akashk@mplgaming.com>
2021-05-22 12:07:42 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ const path = require("path");
 const app = Express();
 
 app.use(Express.static(path.resolve(__dirname, "../frontend/build")));
+
 setupMiddleware(app);
 
 async function startApp() {
   const db = await setupDatabase();
   setupRouter(app, db);
+  // All other GET requests not handled before will return our React app
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
   });
   app.listen(Config.port, () => {
     console.log("Server started at port", Config.port);
